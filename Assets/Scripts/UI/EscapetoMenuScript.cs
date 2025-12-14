a071cc1bef9272007a730fd036547893572f8a44
MoizFawadZaidi <146975597+MoizFawadZaidi@users.noreply.github.com>
2025-12-14 06:58:47 +0000

    Fixed bug with escaping whilst paused

--- Assets/Scripts/UI/EscapetoMenuScript.cs
+++ Assets/Scripts/UI/EscapetoMenuScript.cs
@@ -3,9 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class EscapeToMenu : MonoBehaviour
 {
+    private bool canEscape = true;
+    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Time.timeScale != 1)
+        {
+            canEscape = false;
+        }
+        else
+        {
+            canEscape = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && canEscape)
         {
             SceneManager.LoadScene(0); // 0 = MainMenu in Build Settings
         }
